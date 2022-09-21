c6e8f8ef18871807c24f79367735290e72165899
LeL-nardo <sandroleonardoads@gmail.com>
2022-09-20 22:01:54 -0300

    alpha 2.5

--- public/index.js
+++ public/index.js
@@ -1,17 +1,33 @@
 function Pokemon() {
-    let num = Math.floor(Math.random() * 151) + 1
-    //let num = 6
+    //let num = Math.floor(Math.random() * 151) + 1
+    let num = 6
     const pokeimg = document.getElementsByClassName("pokeimg")
     const pokename = document.getElementById("nomepokemon")
+    const poketype = document.getElementsByClassName("pokename")
+    const list = document.getElementsByClassName("habi-height-list")
+    const altura = document.getElementById("altura-container")
     fetch(`https://pokeapi.co/api/v2/pokemon/${num}/`)
         .then((response) => response.json())
             .then((data) => {
+                // adicionando o gif / imagem
                 pokeimg[0].src = data.sprites.versions["generation-v"]["black-white"].animated["front_default"]
-                console.log(data)
+                
+                // adicionando o nome e id
                 pokename.innerHTML = `${data.id} - ${data.name}`.toUpperCase()
-                if (data["types"].length > 1) {
-                    console.log("HAHAHAHAHAHAHA")
-                } 
+
+                // adicionando habilidades e altura
+                data.abilities.map((item) => {
+                    let li = document.createElement("li")
+                    li.innerHTML = item.ability.name
+                    list[0].appendChild(li)
+                })
+
+                let height = document.createElement("p")
+                height.innerHTML = `${data.height / 10}m`
+                altura.appendChild(height)
+                
+                // adicionando tipo
+                
             })
 }
 Pokemon()
