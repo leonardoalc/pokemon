61d32659da18db4fceb8ce80bed7664168fc82c0
LeL-nardo <sandroleonardoads@gmail.com>
2022-09-22 16:57:18 -0300

    Pokemon v1.0

--- public/index.js
+++ public/index.js
@@ -1,12 +1,33 @@
-function RandomPokemon() {
-    let num = Math.floor(Math.random() * 905) + 1
-    //let num = 899
+function SearchPokemon() {
+    let id = document.getElementById("search")
+    let idfixed = id.value.toLowerCase()
+    const fetchtest = async () => {
+        const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${idfixed}/`)
+        console.log(APIresponse)
+        if (APIresponse.status == 200) {
+            Pokemon(idfixed)
+            id.value = ""
+        } else {
+            alert("ERRO! Id ou Nome inválido... Geraremos um pokemon aleatório.")
+            GenerateId()
+            id.value = ""
+        }
+    }
+    fetchtest()
+}
+function GenerateId(idload=false) {
+    if (idload){
+        Pokemon(idload)   
+    } else {
+        let id = Math.floor(Math.random() * 905) + 1
+        Pokemon(id)
+    }
+}
+function Pokemon(idname) {
     const pokeimg = document.getElementsByClassName("pokeimg")
     const pokename = document.getElementById("nomepokemon")
     const pokestats = document.getElementById("pokehabilidades")
-    const poketype = document.getElementsByClassName("pokename")
-    const altura = document.getElementById("altura-container")
-    fetch(`https://pokeapi.co/api/v2/pokemon/${num}/`)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${idname}/`)
         .then((response) => response.json())
             .then((data) => {
                 // adicionando o gif / imagem
@@ -20,72 +41,39 @@ function RandomPokemon() {
 
                 // adicionando habilidades e altura
                 let cont = 0
+                let uli = document.createElement("ul")
                 data.abilities.map((item) => {
                     if (cont == 0) {
-                        cont = 1
+                        cont++
                         let ul = document.getElementsByClassName("habi-height-list")
                         ul[0].remove()
-                        var uli = document.createElement("ul")
                         uli.setAttribute("class", "habi-height-list")
                         pokestats.appendChild(uli)
                     }
-                    var li = document.createElement("li")
+                    let li = document.createElement("li")
                     li.innerHTML = item.ability.name
                     uli.appendChild(li)
                 })
-                alert("bbbb")
-                let height = document.createElement("p")
-                height.innerHTML = `${data.height / 10}m`
-                height.setAttribute("id", "alturapokemon")
-                altura.appendChild(height)
+
+                let height = document.getElementById("alturapokemon")
+                height.innerHTML = `${(data.height / 10).toFixed(2)}m`
                 
                 // adicionando imagem de tipo
+                cont = 0
+                let divtype = document.createElement("div")
                 data.types.map((tipo) => {
+                    if (cont == 0) {
+                        cont++
+                        let main = document.querySelector("main")
+                        let divtipo = document.getElementsByClassName("pokename")
+                        divtipo[0].remove()
+                        divtype.setAttribute("class", "pokename")
+                        main.appendChild(divtype)
+                    }
                     let img = document.createElement("img")
-                    img.src = `../images/type${tipo.type.name}.png`
                     img.setAttribute("class", "type")
-                    poketype[0].appendChild(img)
-                })
-                
-            })
-}
-function SearchPokemon() {
-    let pokemon = document.getElementById("search")
-    const pokeimg = document.getElementsByClassName("pokeimg")
-    const pokename = document.getElementById("nomepokemon")
-    const poketype = document.getElementsByClassName("pokename")
-    const list = document.getElementsByClassName("habi-height-list")
-    const altura = document.getElementById("altura-container")
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.value}/`)
-        .then((response) => response.json())
-            .then((data) => {
-                // adicionando o gif / imagem
-                if (data.id >= 650) {
-                    pokeimg[0].src = data.sprites["front_default"]
-                } else {
-                    pokeimg[0].src = data.sprites.versions["generation-v"]["black-white"].animated["front_default"]
-                }
-                // adicionando o nome e id
-                pokename.innerHTML = `${data.id} - ${data.name}`.toUpperCase()
-
-                // adicionando habilidades e altura
-                data.abilities.map((item) => {
-                    let li = document.createElement("li")
-                    li.innerHTML = item.ability.name
-                    list[0].appendChild(li)
-                })
-
-                let height = document.createElement("p")
-                height.innerHTML = `${data.height / 10}m`
-                height.setAttribute("id", "alturapokemon")
-                altura.appendChild(height)
-                
-                // adicionando imagem de tipo
-                data.types.map((tipo) => {
-                    let img = document.createElement("img")
                     img.src = `../images/type${tipo.type.name}.png`
-                    img.setAttribute("class", "type")
-                    poketype[0].appendChild(img)
+                    divtype.appendChild(img)
                 })
                 
             })
